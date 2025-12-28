787e75e7243f04842fa2ca4bb54195c2eb004035
Kevin <kevinkouakou113@gmail.com>
2025-12-28 18:06:33 +0000

    feat: package installation and assets importation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+// To use the ScrollTrigger plugin, we need to register thanks to gsap
+gsap.registerPlugin(ScrollTrigger);
+
 const App = () => {
-  return <main>Hello World !</main>;
+  return (
+    <main className="flex items-center justify-center h-screen">
+      Hello World !
+    </main>
+  );
 };
 
 export default App;
